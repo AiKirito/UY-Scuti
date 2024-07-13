fb0f31e984a2c3e79b42eeb6461ae9394178cefd
Your Name <you@example.com>
2024-07-13 18:45:27 +0800

    updated

--- resources/my_tools/ext4_info_get.py
+++ resources/my_tools/ext4_info_get.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 import ext4
@@ -8,176 +7,106 @@ import struct
 import argparse
 from collections import Counter
 
-def get_most_common_context(context_file_path):
-    # 创建一个 Counter 对象来存储每种上下文的出现次数
-    context_counter = Counter()
-
-    # 打开上下文文件
-    with open(context_file_path, 'r') as f:
-        # 遍历文件的每一行
-        for line in f:
-            # 分割每一行以获取上下文
-            _, context = line.strip().split(' ', 1)
-            # 只获取空格右边的第一个内容
-            context = context.split()[0]
-            # 更新上下文的出现次数
-            context_counter[context] += 1
-
-    # 获取出现次数最多的上下文
-    most_common_context, _ = context_counter.most_common(1)[0]
-
-    return most_common_context
-
-# 创建一个解析器
-parser = argparse.ArgumentParser(description='处理 ext4 镜像文件并生成配置文件。')
-parser.add_argument('image_path', type=str, help='ext4 镜像文件的路径')
-parser.add_argument('output_dir', type=str, help='输出文件的目录')
-
-# 解析命令行参数
-if len(sys.argv) < 3:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-
-args = parser.parse_args()
-
-# 从命令行参数中获取 ext4 镜像文件的路径和输出文件的目录
-image_path = args.image_path
-output_dir = args.output_dir
-
-# 获取文件名作为前缀
-prefix = os.path.basename(image_path).split('.')[0]
-
-# 为输出文件生成一个唯一的文件名
-config_output_path = os.path.join(output_dir, f"{prefix}_fs_config")
-context_output_path = os.path.join(output_dir, f"{prefix}_file_contexts")
-
-# 打开 ext4 镜像文件
-f = open(image_path, "rb")
-
-# 创建一个 Volume 对象
-volume = ext4.Volume(f)
-
-# 创建一个空的字典来存储 file_contexts 信息
-file_contexts = {}
-
-# 创建一个空的列表来存储 fs_config 信息
-fs_config = []
-
-# 创建一个栈来保存待处理的目录
-stack = [(volume.root, "")]
-
-# 创建一个集合来保存已经访问过的路径
-visited = set()
-
-while stack:
-    inode, path = stack.pop()
-
-    # 检查路径是否已经访问过
-    if path in visited:
-        continue
-
-    # 将路径添加到已访问集合
-    visited.add(path)
-
-    # 检查 inode 是否是目录
-    if inode.is_dir:  # 使用 inode.is_dir 来获取是否是目录的信息
-        # 遍历这个 inode 的所有目录项
-        for entry in inode.open_dir():
-            # 忽略名称为 "." 或 ".." 的目录项
-            if entry[0] in [".", ".."]:
-                continue
-
-            # 获取这个目录项的完整路径
-            if path == "/":
-                full_path = path + entry[0]   # 使用 entry[0] 来获取文件名
-            else:
-                full_path = path + "/" + entry[0]
-
-            # 获取这个目录项的 inode 对象
-            sub_inode = volume.get_inode(entry[1], entry[2])  # 使用 entry[1] 和 entry[2] 来获取 inode
-
-            # 检查这个目录项是否是符号链接
-            if sub_inode.inode.i_links_count == 0:
-                # 获取符号链接指向的路径
-                link_target = sub_inode.read().decode('utf-8')
-                # 将这个路径添加到处理栈中
-                stack.append((volume.get_inode_by_path(link_target), full_path + '/' + link_target))
-            else:
-                # 获取文件或目录的信息
+def main():
+    parser = argparse.ArgumentParser(description='Process ext4 image files and generate configuration files.')
+    parser.add_argument('image_path', type=str, help='Path to the ext4 image file')
+    parser.add_argument('output_dir', type=str, help='Directory for output files')
+    if len(sys.argv) < 3:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    image_path = args.image_path
+    output_dir = args.output_dir
+    prefix = os.path.basename(image_path).split('.')[0]
+    config_output_path = os.path.join(output_dir, f"{prefix}_fs_config")
+    context_output_path = os.path.join(output_dir, f"{prefix}_file_contexts")
+    f = open(image_path, "rb")
+    volume = ext4.Volume(f)
+    file_contexts = {}
+    fs_config = []
+    stack = [(volume.root, "")]
+    visited = set()
+    while stack:
+        inode, path = stack.pop()
+        for xattr in inode.xattrs():
+            if xattr[0] == "security.selinux":
+                file_contexts[path] = xattr[1]
+                if path == "":
+                    file_contexts["/"] = xattr[1]
+        if path in visited:
+            continue
+        visited.add(path)
+        if inode.is_dir:
+            for entry in inode.open_dir():
+                if entry[0] in [".", ".."]:
+                    continue
+                if path == "/":
+                    full_path = path + entry[0]
+                else:
+                    full_path = path + "/" + entry[0]
+                sub_inode = volume.get_inode(entry[1], entry[2])
+                link_target = ""
+                if sub_inode.is_symlink:
+                    try:
+                        link_target = sub_inode.open_read().read().decode('utf-8')
+                    except Exception as e:
+                        print(f'Error reading symlink target: {e}')
                 owner = sub_inode.inode.i_uid
                 group = sub_inode.inode.i_gid
-                mode = sub_inode.inode.i_mode & 0o777  # 只保留权限位
-                capabilities = None  # 假设没有特殊的能力
-
-                # 获取这个目录项的 SELinux
-                for xattr in sub_inode.xattrs():  # 使用 sub_inode.xattrs() 来获取所有的扩展属性
+                mode = sub_inode.inode.i_mode & 0o777
+                capabilities = ""
+                for xattr in sub_inode.xattrs():
                     if xattr[0] == "security.selinux":
-                        # 将这个文件的 SELinux 存储到字典中
                         file_contexts[full_path] = xattr[1]
                     elif xattr[0] == "security.capability":
-                        # 将这个文件的特殊能力存储到字典中
                         r = struct.unpack('<5I', xattr[1])
                         if r[1] > 65535:
                             cap = hex(int(f'{r[3]:04x}{r[1]:04x}', 16)).upper()
                         else:
                             cap = hex(int(f'{r[3]:04x}{r[2]:04x}{r[1]:04x}', 16)).upper()
                         capabilities = f"capabilities={cap}"
-
-                # 将信息添加到 fs_config 列表中
-                fs_config.append((full_path, owner, group, mode, capabilities))
-
-                # 如果这个目录项是一个目录，将它添加到栈中以便后续处理
+                fs_config.append((full_path, owner, group, mode, capabilities, link_target))
                 if sub_inode.is_dir:
                     stack.append((sub_inode, full_path))
                     for sub_entry in sub_inode.open_dir():
                         if sub_entry[0] not in ['.', '..']:
-                            sub_sub_inode = volume.get_inode(sub_entry[1], sub_entry[2])  # 获取子目录的 inode
-                            stack.append((sub_sub_inode, full_path + '/' + sub_entry[0]))  # 将子目录添加到栈中
+                            sub_sub_inode = volume.get_inode(sub_entry[1], sub_entry[2])
+                            stack.append((sub_sub_inode, full_path + '/' + sub_entry[0]))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(context_output_path, "w") as f:
+        for path, context in file_contexts.items():
+            escaped_path = re.escape(path)
+            f.write(f"/{prefix}{escaped_path} {context.decode('utf8', errors='replace')}\n")
+    with open(config_output_path, "w") as f:
+        for path, owner, group, mode, capabilities, link_target in fs_config:
+            output = f"{prefix}{path} {owner} {group} 0{mode:o} {capabilities} {link_target}"
+            f.write(output.rstrip() + "\n")
+    with open(context_output_path, "r") as f:
+        lines = f.readlines()
+    prefix_permission = None
+    prefix_line = "/" + prefix
+    for line in lines:
+        if line.startswith(prefix_line):
+            prefix_permission = line[len(prefix_line):].strip()
+            break
+    if prefix_permission is not None:
+        lines.insert(0, f"/ {prefix_permission}\n")
+    with open(context_output_path, "w") as f:
+        f.writelines(lines)
+    with open(config_output_path, 'r') as f:
+        lines = f.readlines()
+    if prefix == "vendor":
+        lines.insert(0, f"/ 0 2000 0755\n")
+        lines.insert(1, f"{prefix} 0 2000 0755\n")
+        lines.insert(2, f"{prefix}/ 0 2000 0755\n")
+    else:
+        lines.insert(0, f"/ 0 0 0755\n")
+        lines.insert(1, f"{prefix} 0 0 0755\n")
+        lines.insert(2, f"{prefix}/ 0 0 0755\n")
+    with open(config_output_path, 'w') as f:
+        f.writelines(lines)
+    f.close()
 
-# 检查 output_dir 是否存在，如果不存在，就创建它
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-# 将 file_contexts 信息输出到一个文本文件中
-with open(context_output_path, "w") as f:
-    for path, context in file_contexts.items():
-        escaped_path = re.escape(path)  # 对路径中的特殊字符进行转义
-        f.write(f"/{prefix}{escaped_path} {context.decode('utf8', errors='replace')}\n")
-
-# 将 fs_config 信息输出到一个文本文件中
-with open(config_output_path, "w") as f:
-    for path, owner, group, mode, capabilities in fs_config:
-        if capabilities is not None:
-            f.write(f"{prefix}{path} {owner} {group} 0{mode:o} {capabilities}\n")
-        else:
-            f.write(f"{prefix}{path} {owner} {group} 0{mode:o}\n")
-
-# 获取出现次数最多的上下文
-most_common_context = get_most_common_context(context_output_path)
-
-with open(context_output_path, 'r') as f:
-    lines = f.readlines()
-
-# 在列表的开始处添加一行，表示根目录/的上下文
-lines.insert(0, f"/ {most_common_context}\n")
-lines.insert(1, f"/{prefix} {most_common_context}\n")
-lines.insert(2, f"/{prefix}/ {most_common_context}\n")
-
-# 将修改后的列表写回到上下文文件中
-with open(context_output_path, 'w') as f:
-    f.writelines(lines)
-
-with open(config_output_path, 'r') as f:
-    lines = f.readlines()
-
-# 在列表的开始处添加新的行
-lines.insert(0, f"/ 0 0 0755\n")
-lines.insert(1, f"/{prefix} 0 0 0755\n")
-lines.insert(2, f"/{prefix}/ 0 0 0755\n")
-
-# 将修改后的列表写回到配置文件中
-with open(config_output_path, 'w') as f:
-    f.writelines(lines)
-
-# 在所有操作完成后关闭文件
-f.close()
+if __name__ == "__main__":
+    main()
--- resources/my_tools/flash_tool/FlashROM.bat
+++ resources/my_tools/flash_tool/FlashROM.bat
@@ -13,79 +13,53 @@ for /f "tokens=2 delims=:" %%a in ('chcp') do set "locale=%%a"
 cls
 
 if "!locale!"==" 936" (
-    set "title=Fastboot 刷入工具"
-    set "one_title={F9}            Powered By {F0}Garden Of Joy            {#}{#}{\n}"
-    set "keep_data_flash={04}[1] {01}保留全部数据并刷入{#}{#}{\n}"
-    set "format_data_flash={04}[2] {01}格式化用户数据并刷入{#}{#}{\n}"
-    set "select_project=请选择你要操作的项目："
-    set "waiting_device={0D}————  正在等待设备  ————{#}{\n}{\n}"
-    set "loaded_device=已加载设备："
-    set "detected_compressed_file=检测到压缩文件，正在解压："
-    set "disabled_avb_verification=已禁用 Avb2.0 校验"
-    set "kept_data_reboot=已保留全部数据，准备重启！"
-    set "formatting_data=正在格式化 DATA"
-    set "execution_completed=执行完成，等待自动重启"
-    set "retry_message=重试..."
-    set "success_status=刷入成功"
-    set "failure_status=刷入失败"
+    set "confirm_switch={08}{\n}你确定要执行这个操作吗？{\n}{\n}如果确定，请输入 'I am sure'，否则，输入任意以退出：{\n}{\n}"
     set "device_mismatch_msg=此 ROM 仅适配 !right_device! ，但你的设备是 !DeviceCode!"
-) else (
-    set "title=Fastboot Flash Tool"
+    set "disabled_avb_verification=已禁用 Avb2.0 校验"
+    set "exit_program={04}{\n}[4] {01}退出程序{#}{#}{\n}"
+    set "execution_completed=执行完成，等待自动重启"
+    set "failure_status=刷入失败"
+    set "fastboot_mode={06}当前所处状态：Fastboot 模式{\n}"
+    set "fastbootd_mode={06}当前所处状态：Fastbootd 模式{\n}"
+    set "format_data_flash={04}{\n}[2] {01}格式化用户数据并刷入{#}{#}{\n}"
+    set "formatting_data=正在格式化 DATA"
+    set "keep_data_flash={04}{\n}[1] {01}保留全部数据并刷入{#}{#}{\n}"
+    set "kept_data_reboot=已保留全部数据，准备重启！"
     set "one_title={F9}            Powered By {F0}Garden Of Joy            {#}{#}{\n}"
-    set "keep_data_flash={04}[1]  {01}Keep all data and flash{#}{#}{\n}"
-    set "format_data_flash={04}[2]  {01}Format user data and flash{#}{#}{\n}"
-    set "select_project=Please select the project you want to operate:"
-    set "waiting_device={0D}————  Waiting for device  ————{#}{\n}{\n}"
-    set "loaded_device=Loaded device: "
-    set "detected_compressed_file=Detected compressed file, decompressing: "
-    set "disabled_avb_verification=Disabled Avb2.0 verification"
-    set "kept_data_reboot=Kept all data, preparing to reboot！"
-    set "formatting_data=Formatting DATA"
+    set "retry_message=重试..."
+    set "select_project={02}请选择你要操作的项目：{\n}{\n}"
+    set "success_status=刷入成功"
+    set "switch_to_fastboot={04}{\n}[3] {01}切换到 Fastboot 模式{#}{#}{\n}"
+    set "switch_to_fastbootd={04}{\n}[3] {01}切换到 Fastbootd 模式{#}{#}{\n}"
+    set "title=盾牌座 UY 线刷工具"
+    set "waiting_device={0D}————  正在等待设备  ————{#}{\n}{\n}"
+) else (
+    set "confirm_switch={08}{\n}Are you sure you want to perform this operation?{\n}{\n}If sure, please enter 'I am sure', otherwise, enter anything to exit：{\n}{\n}"
+    set "device_mismatch_msg=This ROM is only compatible with !right_device! , but your device is !DeviceCode!"
+    set "disabled_avb_verification=Avb2.0 verification has been disabled"
+    set "exit_program={04}{\n}[4] {01}Exit program{#}{#}{\n}"
     set "execution_completed=Execution completed, waiting for automatic reboot"
-    set "retry_message=Retrying..."
-    set "success_status=Flash successful"
     set "failure_status=Flash failed"
-    set "device_mismatch_msg=This ROM is only compatible with !right_device!, but your device is !DeviceCode!"
+    set "fastboot_mode={06}Current status: Fastboot mode{\n}"
+    set "fastbootd_mode={06}Current status: Fastbootd mode{\n}"
+    set "format_data_flash={04}{\n}[2] {01}Format user data and flash{#}{#}{\n}"
+    set "formatting_data=Formatting DATA"
+    set "keep_data_flash={04}{\n}[1] {01}Keep all data and flash{#}{#}{\n}"
+    set "kept_data_reboot=All data has been kept, ready to reboot!"
+    set "one_title={F9}            Powered By {F0}Garden Of Joy            {#}{#}{\n}"
+    set "retry_message=Retry..."
+    set "select_project={02}Please select the project you want to operate：{\n}{\n}"
+    set "success_status=Flash successful"
+    set "switch_to_fastboot={04}{\n}[3] {01}Switch to Fastboot mode{#}{#}{\n}"
+    set "switch_to_fastbootd={04}{\n}[3] {01}Switch to Fastbootd mode{#}{#}{\n}"
+    set "title=UY Scuti Flash Tool"
+    set "waiting_device={0D}————  Waiting for device  ————{#}{\n}{\n}"
 )
 
 title !title!
-:HOME
-cls
-
-REM 检查是否存在 .zst 文件
-set "zst_exist="
-for %%a in (images\*.zst) do set "zst_exist=1"
-if defined zst_exist (
-    for /f "delims=" %%a in ('dir /b "images\*.zst"') do (
-        if exist "images\%%~nxa" (
-            echo !detected_compressed_file!%%~na
-            zstd --rm -d images\%%~nxa -o images\%%~na
-        )
-    )
-	echo.
-)
-
-cho {F9}                                                {\n}
-cho !one_title!
-cho {F9}                                                {\n}{\n}
-cho !keep_data_flash!
-cho !format_data_flash!
-ECHO.
-
-set /p zyxz=!select_project!
-if "!zyxz!" == "1" (
-    set xz=1
-    goto FLASH
-) else if "!zyxz!" == "2" (
-    set xz=2
-    goto FLASH
-)
-goto HOME&pause
-
-:FLASH
-cls 
 
 REM 显示等待设备的消息
+echo.
 cho !waiting_device!
 
 REM 获取设备型号
@@ -94,29 +68,104 @@ for /f "tokens=2" %%a in ('fastboot getvar product 2^>^&1^|find "product"') do (
 )
 REM 获取设备的分区类型
 for /f "tokens=2" %%a in ('fastboot getvar slot-count 2^>^&1^|find "slot-count" ') do (
-    set fqlx=%%a
+    set DynamicPartitionType=%%a
 )
-
-REM 根据设备的分区类型设置变量 fqlx 的值
-if "!fqlx!" == "2" (
-    set fqlx=AB
+REM 根据设备的分区类型设置变量 DynamicPartitionType 的值
+if "!DynamicPartitionType!" == "2" (
+    set DynamicPartitionType=NonOnlyA
 ) else (
-    set fqlx=A
+    set DynamicPartitionType=OnlyA
+)
+REM 获取设备的 Fastboot 状态
+for /f "tokens=2" %%a in ('fastboot getvar is-userspace 2^>^&1^|find "is-userspace"') do (
+    set FastbootState=%%a
+)
+REM 根据设备的 Fastboot 状态设置变量 FastbootState 的值
+if "!FastbootState!" == "yes" (
+    set FastbootState=!fastbootd_mode!
+) else (
+    set FastbootState=!fastboot_mode!
 )
 
 cls
-ECHO.!loaded_device!!DeviceCode!
-ECHO.
+
+echo.
 if not "!DeviceCode!"=="!right_device!" (
     cho !device_mismatch_msg!
     PAUSE
     GOTO :EOF
 )
 
+cls
+
+cho {F9}                                                {\n}
+cho !one_title!
+cho {F9}                                                {\n}{\n}
+cho !FastbootState!
+cho !keep_data_flash!
+cho !format_data_flash!
+if "!FastbootState!" == "!fastbootd_mode!" (
+    cho !switch_to_fastboot!
+) else (
+    cho !switch_to_fastbootd!
+)
+cho !exit_program!
+echo.
+
+cho !select_project!
+set /p UserChoice=
+if "!UserChoice!" == "1" (
+    set SelectedOption=1
+    goto FLASH
+) else if "!UserChoice!" == "2" (
+    set SelectedOption=2
+    goto FLASH
+) else if "!UserChoice!" == "3" (
+    cho !confirm_switch!
+    set /p Confirmation=
+    if /I "!Confirmation!" == "I am sure" (
+        if "!FastbootState!" == "!fastbootd_mode!" (
+            cls
+            echo.
+            fastboot reboot bootloader
+        ) else (
+            cls
+            echo.
+            fastboot reboot fastboot
+        )
+    )
+    goto HOME
+) else if "!UserChoice!" == "4" (
+    exit
+)
+goto HOME&pause
+
+:FLASH
+cls 
+echo.
+
+REM 对 vbmeta 分区文件专门刷入
+set "count=0"
+for /R images\ %%i in (*.img) do (
+	echo %%~ni | findstr /B "vbmeta" >nul && (
+		if "!DynamicPartitionType!"=="OnlyA" (
+			fastboot --disable-verity --disable-verification flash %%~ni %%i
+		) else (
+			fastboot --disable-verity --disable-verification flash %%~ni_a %%i
+			fastboot --disable-verity --disable-verification flash %%~ni_b %%i
+		)
+		set /a "count+=1"
+	)
+)
+if !count! gtr 0 (
+	echo !disabled_avb_verification!
+	echo.
+)
+
 REM 遍历分区文件并刷入
-for /f "delims=" %%b in ('dir /b images ^| findstr /v /i "super.img" ^| findstr /v /i "preloader_raw.img" ^| findstr /v /i "cust.img" ^| findstr /v /i "recovery.img" ^| findstr /v /i /b "vbmeta"') do (
+for /f "delims=" %%b in ('dir /b images\*.img ^| findstr /v /i "super.img" ^| findstr /v /i "preloader_raw.img" ^| findstr /v /i "cust.img" ^| findstr /v /i "recovery.img" ^| findstr /v /i /b "vbmeta"') do (
     set "filename=%%~nb"
-    if "!fqlx!"=="A" (
+    if "!DynamicPartitionType!"=="OnlyA" (
         set "retry=0"
         :retryA
         "fastboot" flash %%~nb images\%%~nxb
@@ -170,38 +219,26 @@ if exist images\preloader_raw.img (
     	fastboot flash preloader2 images\preloader_raw.img !sg!
 	echo.
 )
-REM 对特定分区文件专门刷入
-set "count=0"
-for /R images\ %%i in (*.img) do (
-	echo %%~ni | findstr /B "vbmeta" >nul && (
-		fastboot --disable-verity --disable-verification flash %%~ni_a %%i
-		fastboot --disable-verity --disable-verification flash %%~ni_b %%i
-		set /a "count+=1"
-	)
-)
-if !count! gtr 0 (
-	echo !disabled_avb_verification!
-	echo.
-)
 
 if exist images\cust.img (
 	fastboot flash cust images\cust.img
 	echo.
 )
+
 if exist images\super.img (
     	fastboot flash super images\super.img
 	echo.
 )
 
-if "!xz!" == "1" (
+if "!SelectedOption!" == "1" (
     echo !kept_data_reboot!
-) else if "!xz!" == "2" (
+) else if "!SelectedOption!" == "2" (
     echo !formatting_data!
     fastboot erase userdata
     fastboot erase metadata
 )
 
-if "!fqlx!" == "AB" (
+if "!DynamicPartitionType!" == "NonOnlyA" (
     fastboot set_active a %sg%
 )
 
